a8423c2e6eb109b168c732542f8b1df2b560c10a
arminasy <57625772+arminasy@users.noreply.github.com>
2021-04-13 15:12:25 +0400

    updated without 42 flag

---
+++ ft_strnew.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+char    *ft_strnew(size_t size)
+{
+  char  *str;
+  if (!(str = (char *)malloc(sizeof(char) * size + 1)))
+    return (NULL);
+  str[size] = '\0';
+  while (size--)
+    str[size] = '\0';
+  return (str);
+}
